08766fa55bca68cbf16ae3e9d111e0cb253b26d1
rodribp <rodrigobonilla177@gmail.com>
2023-12-25 12:50:54 -0600

    bug of domain solved we'll have to change it for production

--- src/js/script.js
+++ src/js/script.js
@@ -43,7 +43,7 @@ async function changeLanguage(page, language, callback) {
   let res = "";
 
   // If content does not exist or does not have a valid 'secret', fetch and store it
-  if (window.location.pathname != "/src/index.html") {
+  if (window.location.pathname != "/src/index.html" || window.location.pathname != "/src/") {
       res = await fetch(`../content/content-${language}.json`);
   } else {
       res = await fetch(`./content/content-${language}.json`);
