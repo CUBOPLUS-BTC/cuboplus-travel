9b676327a50c5842fd8f9d22d022fcb0adc64652
rodrigoamd <rodrigoaguilerad2000@gmail.com>
2024-02-06 19:12:41 -0600

    Uploading testimonial slider

---
+++ src/js/carousel.js
@@ -0,0 +1,33 @@
+var swiper = new Swiper('.mySwiper', {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  loop: true,
+  fade: true,
+  grabCursor: true,
+  autoplay: {
+    delay: 5000,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 'auto',
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 'auto',
+      spaceBetween: 20,
+    },
+    992: {
+      slidesPerView: 'auto',
+      spaceBetween: 20,
+    },
+  },
+});
