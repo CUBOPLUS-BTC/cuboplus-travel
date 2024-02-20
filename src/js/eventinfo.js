9413e1fe77a3dbbcd29f06e973db4a2d36f52004
rodribp <rodrigobonilla177@gmail.com>
2024-02-19 20:58:30 -0600

    gallery for events created

--- src/js/eventinfo.js
+++ src/js/eventinfo.js
@@ -1,11 +1,46 @@
 const params = new URLSearchParams(window.location.search);
 const uuid = params.get("uuid");
 
+// Function to generate the image gallery
+const generateGallery = async (files) => {
+  let html = "";
+
+  // Function to load images asynchronously and get their dimensions
+  const loadImage = (urlFile) => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.onload = () => {
+        resolve({ url: urlFile, width: img.width, height: img.height });
+      };
+      img.onerror = (error) => reject(error);
+      img.src = urlFile;
+    });
+  };
+
+  // Load all images and get their dimensions
+  const imagesData = await Promise.all(files.map(file => loadImage(URL + "img/events/" + file)));
+
+  // Build HTML with images and their dimensions
+  imagesData.forEach((data, index) => {
+    const { url, width, height } = data;
+    const displayStyle = index === 0 ? '' : 'style="display:none;"'; // Show only the first image
+    html += `<a href="${url}"
+               ${displayStyle}
+               data-pswp-width="${width}" 
+               data-pswp-height="${height}" 
+               target="_blank">
+               <img src="${url}" alt="" />
+             </a>`;
+  });
+
+  // Add the generated HTML to the images container
+  document.getElementById("images").innerHTML = html;
+}
+
 const fillEvent = (data) => {
   
     document.getElementById("meetup-title").innerText = data.title;
     document.getElementById("meetup-desc").innerText = data.description;
-    document.getElementById("meetup-img").src = URL + "img/events/" + data.fileName;
     document.getElementById("meetup-hour").innerText = data.timeStart + " - " + data.timeEnd;
     document.getElementById("meetup-date").innerText = data.start === data.end ? data.start : data.start + " - " + data.end;
     document.getElementById("meetup-place").innerText = data.location;
@@ -37,6 +72,7 @@ document.addEventListener("DOMContentLoaded", async (e) => {
       }
 
       fillEvent(result.data);
+      generateGallery(result.data.files);
 });
 
 esButton[0].addEventListener("click", (e) => {
