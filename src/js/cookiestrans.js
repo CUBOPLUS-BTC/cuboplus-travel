0ea0b3571a663b8dbb473e6c92f288dc5c98f595
OscarG673 <oguevara1969@gmail.com>
2024-02-26 14:35:16 -0600

    fixing translations

--- src/js/cookiestrans.js
+++ src/js/cookiestrans.js
@@ -12,6 +12,34 @@ const setElements = (content) => {
     document.getElementById("cookies-desc5").innerHTML = content.descriptions[4];
     document.getElementById("cookies-desc6").innerHTML = content.descriptions[5];
     document.getElementById("cookies-desc7").innerHTML = content.descriptions[6];
+    document.getElementById("table1").innerHTML = content.tableEssen[0];
+    document.getElementById("table2").innerHTML = content.tableEssen[1];
+    document.getElementById("table3").innerHTML = content.tableEssen[2];
+    document.getElementById("table4").innerHTML = content.tableEssen[3];
+    document.getElementById("table-info1").innerHTML = content.tableEssenData[0];
+    document.getElementById("table-info2").innerHTML = content.tableEssenData[1];
+    document.getElementById("table-info9").innerHTML = content.tableEssenData[1];
+    document.getElementById("table-info3").innerHTML = content.tableEssenData[2];
+    document.getElementById("table-info4").innerHTML = content.tableEssenData[3];
+    document.getElementById("table-info5").innerHTML = content.tableEssenData[4];
+    document.getElementById("table-info6").innerHTML = content.tableEssenData[5];
+    document.getElementById("table-info7").innerHTML = content.tableEssenData[5];
+    document.getElementById("table-exp1").innerHTML = content.tableEssenExp[0];
+    document.getElementById("table-exp9").innerHTML = content.tableEssenExp[0];
+    document.getElementById("table-exp0").innerHTML = content.tableEssenExp[1];
+    document.getElementById("table-exp2").innerHTML = content.tableEssenExp[1];
+    document.getElementById("table-exp3").innerHTML = content.tableEssenExp[2];
+    document.getElementById("table-exp4").innerHTML = content.tableEssenExp[3];
+    document.getElementById("table-exp5").innerHTML = content.tableEssenExp[3];
+    document.getElementById("table-exp6").innerHTML = content.tableEssenExp[3];
+    document.getElementById("table-type1").innerHTML = content.tableEssenTyp[0];
+    document.getElementById("table-type2").innerHTML = content.tableEssenTyp[0];
+    document.getElementById("table-type3").innerHTML = content.tableEssenTyp[0];
+    document.getElementById("table-type4").innerHTML = content.tableEssenTyp[0];
+    document.getElementById("table-type5").innerHTML = content.tableEssenTyp[1];
+    document.getElementById("table-type6").innerHTML = content.tableEssenTyp[1];
+    document.getElementById("table-type7").innerHTML = content.tableEssenTyp[1];
+
 }
 
 document.addEventListener("DOMContentLoaded", (e) => {
