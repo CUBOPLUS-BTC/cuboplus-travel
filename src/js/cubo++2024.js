fe9dc490b43981693592ebe67c5df3e7a0963aa2
rodribp <rodrigobonilla177@gmail.com>
2024-03-09 00:42:17 -0600

    translation for devs gen done

--- src/js/cubo++2024.js
+++ src/js/cubo++2024.js
@@ -2,8 +2,13 @@ const setElements = (content) => {
     document.getElementById("title").innerHTML = content.header;
     document.getElementById("legend").innerHTML = content.legend;
     document.getElementById("ask").innerHTML = content.form;
-    document.getElementById("description").innerHTML = content.description;
-    document.getElementById("thanks").innerHTML = content.alliances;
+    document.getElementById("question1").innerHTML = content.questions[0];
+    document.getElementById("question2").innerHTML = content.questions[1];
+    document.getElementById("advantages-1").innerHTML = content.advantages[0];
+    document.getElementById("advantages-2").innerHTML = content.advantages[1];
+    document.getElementById("advantages-3").innerHTML = content.advantages[2];
+    document.getElementById("call").innerHTML = content.callAction[0];
+    document.getElementById("action").innerHTML = content.callAction[1];
 }
 
 document.addEventListener("DOMContentLoaded", (e) => {
