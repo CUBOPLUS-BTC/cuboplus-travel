794599f016dbba8f5ec83b62736d029224a04d21
rodribp <rodrigobonilla177@gmail.com>
2024-03-08 18:07:51 -0600

    fixed index translation, fixing cubo+ logo in all the pages excepting cubo+ 2024 and cubo++

--- src/js/component.js
+++ src/js/component.js
@@ -47,10 +47,10 @@ async function changeLanguage(page, language, callback) {
   // If content does not exist or does not have a valid 'secret', fetch and store it
   if (page !== "index" && page === "cookies") {
       res = await fetch(`../../content/content-${language}.json`);
-  } else if (page !== "cookies") {
+  } else if (page !== "cookies" && page !== "index") {
       res = await fetch(`../content/content-${language}.json`);
   } else {
-      res = await fetch(`./content/content-${language}.json`);
+      res = await fetch(`content/content-${language}.json`);
   }
   const res1 = await res.json();
 
