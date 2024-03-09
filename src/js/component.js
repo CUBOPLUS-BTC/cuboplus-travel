0eb52359a3e2077bd882d62ca0149264a19f20db
rodribp <rodrigobonilla177@gmail.com>
2024-03-08 18:01:36 -0600

    fixing translation in cookies page

--- src/js/component.js
+++ src/js/component.js
@@ -45,7 +45,9 @@ async function changeLanguage(page, language, callback) {
   let res = "";
 
   // If content does not exist or does not have a valid 'secret', fetch and store it
-  if (page != "index") {
+  if (page !== "index" && page === "cookies") {
+      res = await fetch(`../../content/content-${language}.json`);
+  } else if (page !== "cookies") {
       res = await fetch(`../content/content-${language}.json`);
   } else {
       res = await fetch(`./content/content-${language}.json`);
