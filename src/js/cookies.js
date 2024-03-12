2e85f0a8bf56904fb6318bfd2fd861f64aa7cfd7
rodribp <rodrigobonilla177@gmail.com>
2024-03-11 21:37:40 -0600

    fixing cookies modal translation

--- src/js/cookies.js
+++ src/js/cookies.js
@@ -5,14 +5,45 @@ function closeModal() {
     modalEl.style.display = "none";
 }
 
+const setCookiesElements = (content) => {
+    document.getElementById("cookies-text").innerHTML = content[0];
+    document.getElementById("accept-cookies").innerHTML = content[1];
+    document.getElementById("decline-cookies").innerHTML = content[2];
+}
 
 document.addEventListener("DOMContentLoaded", (e) => {
+    changeLanguage("cookiesmodal", localStorage.getItem("language") || "en", (content) => {
+        setCookiesElements(content);
+    });
 
     const cookiesAgreed = localStorage.getItem("cookies-agreed");
     if (cookiesAgreed == null) {
         modalEl.style.display = "flex";
         return;
     }
+});
+esButton[0].addEventListener("click", (e) => {
+    changeLanguage("cookiesmodal", "es", (content) => {
+        setCookiesElements(content);
+    });
+});
+
+esButton[1].addEventListener("click", (e) => {
+    changeLanguage("cookiesmodal", "es", (content) => {
+        setCookiesElements(content);
+    });
+});
+
+enButton[0].addEventListener("click", (e) => {
+    changeLanguage("cookiesmodal", "en", (content) => {
+        setCookiesElements(content);
+    });
+});
+
+enButton[1].addEventListener("click", (e) => {
+    changeLanguage("cookiesmodal", "en", (content) => {
+        setCookiesElements(content);
+    });
 });
 
 document.getElementById("accept-cookies").addEventListener("click", (e) => {
