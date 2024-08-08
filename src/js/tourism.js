c1752039d2093978c69990b6a1d6c66d56ca4139
rodribp <rodrigobonilla177@gmail.com>
2024-08-08 08:31:27 -0600

    removing changes and adding president quote

--- src/js/tourism.js
+++ src/js/tourism.js
@@ -1,12 +1,5 @@
 const address = "32ixEdVJWo3kmvJGMTZq5jAQVZZeuwnqzo";
-const weekSeconds = 604800;
-const monthSeconds = 2629746;
-const currentDateUnix = Math.floor(Date.now() / 1000);
-const lastMonth = currentDateUnix - monthSeconds;
-const lastWeek = currentDateUnix - weekSeconds;
 const balanceElement = document.getElementById("balance-number");
-const changeWeekElement = document.getElementById("change-7d-number");
-const changeMonthElement = document.getElementById("change-30d-number");
 
 const getBalance = async () => {
     const result = await fetch(`https://bitcoin.gob.sv/api/address/${address}`);
@@ -15,29 +8,12 @@ const getBalance = async () => {
     return balance;
 }
 
-const getChanges = async () => {
-    const result = await fetch(`https://bitcoin.gob.sv/api/address/${address}/txs/summary`);
-    const response = await result.json();
-
-    let valueLastWeek = 0;
-    let valueLastMonth = 0;
-
-    for (let i = 0; i < response.length; i++) {
-        valueLastWeek += response[i].time > lastWeek ? response[i].value : 0;
-        valueLastMonth += response[i].time > lastMonth ? response[i].value : 0;
-    }
-
-    const changes = [Math.floor(valueLastWeek / 100000000, 2), Math.floor(valueLastMonth / 100000000)];
-
-    return changes;
-}
-
-const animateValue = (element, start, end, duration, prefix = '') => {
+const animateValue = (element, start, end, duration) => {
     let startTimestamp = null;
     const step = (timestamp) => {
         if (!startTimestamp) startTimestamp = timestamp;
         const progress = Math.min((timestamp - startTimestamp) / duration, 1);
-        element.innerHTML = prefix + Math.floor(progress * (end - start) + start);
+        element.innerHTML = Math.floor(progress * (end - start) + start);
         if (progress < 1) {
             window.requestAnimationFrame(step);
         }
@@ -47,9 +23,6 @@ const animateValue = (element, start, end, duration, prefix = '') => {
 
 document.addEventListener("DOMContentLoaded", async (e) => {
     const balance = await getBalance();
-    const changes = await getChanges();
 
     animateValue(balanceElement, 0, balance, 1500);
-    animateValue(changeWeekElement, 0, changes[0], 1500, '+');
-    animateValue(changeMonthElement, 0, changes[1], 1500, '+');
 });
