8779c245d3e510ca163a00334054220d5197df04
OscarG673 <oguevara1969@gmail.com>
2024-10-29 13:06:37 -0600

    fixing bitcoin balance

--- src/js/tourism.js
+++ src/js/tourism.js
@@ -4,28 +4,31 @@ const balanceElement = document.getElementById("balance-number");
 const getBalance = async () => {
     const result = await fetch(`https://mempool.space/api/address/${address}`);
     const response = await result.json();
-    const balance = response.chain_stats.funded_txo_sum / 100000000;
+
+    const balanceSats= response.chain_stats.funded_txo_sum - response.chain_stats.spent_txo_sum;
+    const balance = balanceSats / 100000000; 
+
     return balance;
 }
 
 const animateValue = (element, start, end, duration) => {
     let startTimestamp = null;
 
-    // Función de easing personalizada con potencia mayor para un ease-out más fuerte
+      // Función de easing personalizada con potencia mayor para un ease-out más fuerte
     const easeOutPower = (t) => 1 - Math.pow(1 - t, 5);
 
     const step = (timestamp) => {
         if (!startTimestamp) startTimestamp = timestamp;
         const elapsed = (timestamp - startTimestamp) / duration;
         const progress = Math.min(easeOutPower(elapsed), 1);
-        const currentValue = (progress * (end - start) + start).toFixed(2); // Keep two decimal places
+        const currentValue = (progress * (end - start) + start).toFixed(2);
 
-        element.innerHTML = currentValue;
+        element.innerHTML = `${currentValue} BTC`; 
 
         if (elapsed < 1) {
             window.requestAnimationFrame(step);
         } else {
-            element.innerHTML = end.toFixed(2); // Ensure final value has two decimal places
+            element.innerHTML = `${end.toFixed(2)} BTC`; // Ensure final value has two decimal places
         }
     };
 
@@ -34,9 +37,9 @@ const animateValue = (element, start, end, duration) => {
 
 document.addEventListener("DOMContentLoaded", async (e) => {
     const balance = await getBalance();
-
     animateValue(balanceElement, 0, balance, 1500);
 });
+
 
 document.querySelectorAll('.faq-item').forEach(item => {
     item.addEventListener('click', () => {
